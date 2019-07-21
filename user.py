61760d968f4e9668949f60140f79df09acdadc59
vincentouma <vinceoumah@gmail.com>
2019-07-22 00:41:26 +0300

    Add delete user

--- user.py
+++ user.py
@@ -3,14 +3,24 @@ class User:
     '''
     Class that generates new instances of user
     '''
-    user_list = [] #list of users to be stored here
+    user_list = []  # list of users to be stored here
 
-    def __init__(self,username, password ):
+    def __init__(self, username, password):
         '''
         saving user credentials into user_list for login
         '''
-        self.username=username
-        self.password= password
+        self.username = username
+        self.password = password
+
+#########save multiple users#########
 
     def save_user(self):
         User.user_list.append(self)
+
+        ###########Delete User######
+
+    def delete_user(self):
+        '''
+        delete a user account
+        '''
+        User.user_list.remove(self)
--- user_Test.py
+++ user_Test.py
@@ -44,6 +44,19 @@ class TestUser(unittest.TestCase):
         test_user.save_user()
         self.assertEqual(len(User.user_list), 2)
 
+        ###########4th test#####Delete user#####
+
+    def test_delete_user(self):
+        '''
+        check whether one can delete a user account
+        '''
+        self.new_user.save_user()
+        test_user = User("test", "password")
+        test_user.save_user()
+        self.new_user.delete_user()
+        self.assertEqual(len(User.user_list), 1)
+
+
 if __name__ == '__main__':
   unittest.main()
 
