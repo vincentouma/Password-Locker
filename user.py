46f9adcc41ebc52213fcaa6341592c45346acf14
vincentouma <vinceoumah@gmail.com>
2019-07-21 22:56:24 +0300

    creat new user

--- user.py
+++ user.py
@@ -5,3 +5,22 @@ class User:
     """
 
     user_list = []
+
+    def save_user(self):
+    
+        '''
+        save_user method saves user objects into user_list
+        '''
+
+        User.user_list.append(self)
+
+
+
+    def __init__(self,first_name,last_name,number,email):
+
+      # docstring removed for simplicity
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
--- user_Test.py
+++ user_Test.py
@@ -8,3 +8,24 @@ class TestUsers(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+    #########First test #########
+
+    def setUp(self):
+         #setup method to run before each test cases.
+
+        self.new_user = User ("vinceouma@gmail.com", "vincentouma", "vinceobindi1005")
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.email,"vinceouma@gmail.com")
+        self.assertEqual(self.new_user.username,"vincentouma")
+        self.assertEqual(self.new_user.password,"vinceobindi1005")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
