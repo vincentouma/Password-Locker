e3adb9a7016fc5b68c0624eb279d68beb837430b
vincentouma <vinceoumah@gmail.com>
2019-07-21 22:28:25 +0300

    Add import unitest from class user

---
+++ user.py
@@ -0,0 +1,7 @@
+###########CLASS USER#########
+class User:
+    """
+    Class that generates new instances of users.
+    """
+
+    user_list = []
---
+++ user_Test.py
@@ -0,0 +1,10 @@
+import unittest # Importing the unittest module
+from user import User # Importing the contact class
+
+class TestUsers(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the Users class behaviours.
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
